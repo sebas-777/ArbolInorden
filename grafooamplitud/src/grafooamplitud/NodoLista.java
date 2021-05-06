4be158830161a8617d52faa3c1585b7dda31cd45
sebas-777 <53665624+sebas-777@users.noreply.github.com>
2021-05-05 20:41:12 -0500

    recorre arbol posorden y preorden

---
+++ grafooamplitud/src/grafooamplitud/NodoLista.java
@@ -0,0 +1,11 @@
+package grafooamplitud;
+
+// Creacion de un nodo de una lista
+public class NodoLista {
+public int clave;
+public NodoLista sig;
+public NodoLista(int x, NodoLista n) {
+    clave = x;
+    sig = n;
+}
+}
